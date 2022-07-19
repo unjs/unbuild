e20dd919dc5f4074160f1af96978b2b93c139efb
Daniel Roe <daniel@roe.dev>
2022-07-19 12:44:15 +0100

    feat: enable `esmResolve` when loading `build.config`

--- src/builder/untyped.ts
+++ src/builder/untyped.ts
@@ -14,6 +14,7 @@ export async function typesBuild (ctx: BuildContext) {
   for (const entry of entries) {
     const options = {
       jiti: {
+        esmResolve: true,
         interopDefault: true,
         transformOptions: {
           babel: {
