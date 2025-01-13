cefc220162af2a835e7d3ecafad30c171407a323
userquin <userquin@gmail.com>
2025-01-13 20:12:43 +0100

    chore: include mixed declaration in re-export fixture

--- test/cjs-types-fixture/reexport-types/index.ts
+++ test/cjs-types-fixture/reexport-types/index.ts
@@ -2,6 +2,13 @@ import type { Options } from "./types";
 
 export type * from "./types";
 
-export default function plugin(options: Options = {}): Options {
-  return options;
+export interface ResolvedOptions extends Options {
+  name: string;
+}
+
+export default function plugin(options: Options = {}): ResolvedOptions {
+  return {
+    ...options,
+    name: "plugin",
+  };
 }
