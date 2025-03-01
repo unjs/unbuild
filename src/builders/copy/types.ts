64f6eafcd30eaeb1a3e3254d4215a104cfeab4c7
kricsleo <kricsleo@163.com>
2025-03-01 18:40:56 +0800

    feat(copy): add `copy:entry:options` hook

--- src/builders/copy/types.ts
+++ src/builders/copy/types.ts
@@ -1,4 +1,5 @@
 import type { BaseBuildEntry, BuildContext } from "../../types";
+import type { GlobOptions } from 'tinyglobby'
 
 export interface CopyBuildEntry extends BaseBuildEntry {
   builder: "copy";
@@ -9,6 +10,11 @@ export interface CopyHooks {
   "copy:entries": (
     ctx: BuildContext,
     entries: CopyBuildEntry[],
+  ) => void | Promise<void>;
+  "copy:entry:options": (
+    ctx: BuildContext,
+    entry: CopyBuildEntry,
+    options: GlobOptions,
   ) => void | Promise<void>;
   "copy:done": (ctx: BuildContext) => void | Promise<void>;
 }
